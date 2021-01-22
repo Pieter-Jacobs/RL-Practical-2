09eeaf5df667db135cfd737bc0c493e6c842b538
NBurgler <63599473+NBurgler@users.noreply.github.com>
2021-01-22 16:29:08 +0100

    Fixed Scale

--- graphScriptEpsilon.py
+++ graphScriptEpsilon.py
@@ -4,24 +4,27 @@ import csv
 x0=[]
 y0=[]
 
-with open('sarsaEpsilon.txt', 'r') as csvfile:
+with open('sarsaEpsilon.csv', 'r') as csvfile:
     plots= csv.reader(csvfile, delimiter=',')
     for row in plots:
-        x0.append(int(row[0]))
-        y0.append(int(row[1]))
+        x0.append(float(row[0]))
+        y0.append(float(row[1]))
 
 plt.plot(x0,y0, marker='o')
+plt.ticklabel_format(useOffset=False, style='plain')
 
 x1=[]
 y1=[]
 
-with open('qLearningEpsilon.txt', 'r') as csvfile:
+with open('qLearningEpsilon.csv', 'r') as csvfile:
     plots= csv.reader(csvfile, delimiter=',')
     for row in plots:
-        x1.append(int(row[0]))
-        y1.append(int(row[1]))
+        x1.append(float(row[0]))
+        y1.append(float(row[1]))
 
 plt.plot(x1,y1, marker='o')
+plt.ticklabel_format(useOffset=False, style='plain')
+plt.legend(['Sarsa', 'Q-Learning'])
 
 plt.title('Q-Learning vs Sarsa: Epsilon-Greedy')
 
